bee0820affcc929787c99df740889c3b94d4b3a4
Kaj Beugels <k.beugels@outlook.com>
2025-01-24 20:29:50 +0100

    Add basic folders

---
+++ Assets/Scripts/Bubble.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    [SerializeField,Tooltip("The rigidbody of the bubble")]
+    private new Rigidbody rigidbody;
+
+    [SerializeField,Tooltip("Amount of upwards forces is applied to the bubble each frame")]
+    private float upwardsVelocity;
+
+    
+    private void FixedUpdate()
+    {
+        //Add force to the bubble
+        rigidbody.AddForce(Vector3.up);
+    }
+
+}
