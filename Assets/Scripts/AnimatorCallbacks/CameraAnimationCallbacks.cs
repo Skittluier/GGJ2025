525f76a86cdbbc2b69a246bcb4d0d988b526007e
Maarten Braaksma <maarten@bucketheadentertainment.com>
2025-01-25 21:23:51 +0100

    Iterate intro sequence
    - Let the player cry and stuff

--- Assets/Scripts/AnimatorCallbacks/CameraAnimationCallbacks.cs
+++ Assets/Scripts/AnimatorCallbacks/CameraAnimationCallbacks.cs
@@ -1,10 +1,15 @@
+using SpiritLevel;
 using SpiritLevel.Player;
 using System.Collections;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class CameraAnimationCallbacks : MonoBehaviour
 {
+    [Header("Sounds")]
+    [SerializeField]
+    private AudioClip introSequenceAudioClip;
+
+
     /// <summary>
     /// Callback invoked through animations that the intro animation on the camera is done playing
     /// </summary>
@@ -18,6 +23,7 @@ public class CameraAnimationCallbacks : MonoBehaviour
     /// </summary>
     public void MakeBubbleCry()
     {
+        GlobalAudio.Instance.PlayAudioResource(introSequenceAudioClip);
         Bubble firstPlayerBubble = PlayerManager.Instance?.Players[0]?.Bubble;
 
         if (!firstPlayerBubble)
@@ -27,13 +33,10 @@ public class CameraAnimationCallbacks : MonoBehaviour
 
         IEnumerator CryRoutine()
         {
-            firstPlayerBubble.SetExpression(Bubble.Expression.Crying, 0.5f);
-            yield return new WaitForSeconds(0.75f);
+            firstPlayerBubble.SetExpression(Bubble.Expression.Crying, 14f);
+            yield return new WaitForSeconds(14f);
 
-            firstPlayerBubble.SetExpression(Bubble.Expression.Crying, 0.5f);
-            yield return new WaitForSeconds(0.75f);
-
-            firstPlayerBubble.SetExpression(Bubble.Expression.Crying, 0.5f);
+            firstPlayerBubble.SetExpression(Bubble.Expression.Floating, 2f);
         }
     }
 }
